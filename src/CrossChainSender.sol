e3452ea9a128e68725b3f389f1b0534a5a595483
Bhanu-Partap <bhanupartap25275@gmail.com>
2025-03-17 17:59:11 +0530

    update the file path

--- src/CrossChainSender.sol
+++ src/CrossChainSender.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "lib/wormhole-solidity-sdk/src/WormholeRelayerSDK.sol";
-import "lib/wormhole-solidity-sdk/src/interfaces/token/IERC20.sol";
+import "../lib/wormhole-solidity-sdk/src/WormholeRelayerSDK.sol";
+import "../lib/wormhole-solidity-sdk/src/interfaces/token/IERC20.sol";
 
 contract CrossChainSender is TokenSender {
     uint256 constant GAS_LIMIT = 250_000;
